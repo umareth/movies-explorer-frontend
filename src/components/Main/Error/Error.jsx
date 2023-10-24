218728751c88398af1dfb025951808dc717d2244
Maya <Maya@mail.ru>
2023-10-24 15:36:02 +0300

    исправил ошибку страницы 404

--- src/components/Main/Error/Error.jsx
+++ src/components/Main/Error/Error.jsx
@@ -5,14 +5,14 @@ import { Link } from "react-router-dom";
 
 export default function Error(props) {
   useEffect(() => {
-    props.setIsErrPage(true);
-  }, [props.setIsErrPage]);
+    props.setIsErrorPage(true);
+  }, [props.setIsErrorPage]);
 
   return (
     <Main className="error">
       <h1 className="error__title">404</h1>
       <p className="error__subtitle">Страница не найдена</p>
-      <Link className="error__link" to={-1} onClick={() => props.setIsErrPage(false)}>
+      <Link className="error__link" to={-1} onClick={() => props.setIsErrorPage(false)}>
         Назад
       </Link>
     </Main>
